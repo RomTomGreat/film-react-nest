bb45c6bb9fa7095d3e164f1370cf6a3292863f7a
Tomin Roman <tominroman128@gmail.com>
2025-01-20 17:22:21 +0300

    try connect database

--- backend/src/database/postgres.config.ts
+++ backend/src/database/postgres.config.ts
@@ -8,13 +8,13 @@ export class PostgresConfig implements TypeOrmOptionsFactory {
     createTypeOrmOptions(): TypeOrmModuleOptions {
         return {
             type: 'postgres',
-            host: this.configService.get<string>('DB_HOST', process.env.POSTGRES_HOST),
+            driver: this.configService.get<string>('DB_HOST', process.env.DATABASE_DRIVER),
             port: this.configService.get<number>('DB_PORT', Number(process.env.POSTGRES_PORT)),
             username: this.configService.get<string>('DB_USERNAME', process.env.POSTGRES_USER),
             password: this.configService.get<string>('DB_PASSWORD', process.env.POSTGRES_PASSWORD),
-            database: this.configService.get<string>('DB_DATABASE', process.env.POSTGRES_DB),
+            database: this.configService.get<string>('DB_DATABASE', process.env.DB_NAME),
             entities: [__dirname + '/../**/*.entity{.js,.ts}'],
-            synchronize: true
+            synchronize: false
         };
     }
 };
