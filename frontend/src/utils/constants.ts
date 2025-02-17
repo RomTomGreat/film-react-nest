3ebdc140f13f49f682c4c0807b45df0bde2e47b2
Tomin Roman <tominroman128@gmail.com>
2025-02-17 19:34:05 +0300

    fix dockerfile

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,2 +1,2 @@
-export const API_URL = 'http://romtomgreat.nomorepartiesco.ru/api/afisha';
-export const CDN_URL = 'http://romtomgreat.nomorepartiesco.ru/content/afisha';
+export const API_URL = `http://romtomgreat.nomorepartiesco.ru/api/afisha`;
+export const CDN_URL = `http://romtomgreat.nomorepartiesco.ru/content/afisha`;
